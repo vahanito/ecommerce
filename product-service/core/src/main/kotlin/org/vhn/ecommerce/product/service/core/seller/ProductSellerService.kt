642a85fb99b48f6a566b44517f1c36c2cbb0cdb6
Tomas Vahancik <vhn.448@gmail.com>
2023-03-06 22:24:09 +0100

    Fix optional name param for sellers

--- product-service/core/src/main/kotlin/org/vhn/ecommerce/product/service/core/seller/ProductSellerService.kt
+++ product-service/core/src/main/kotlin/org/vhn/ecommerce/product/service/core/seller/ProductSellerService.kt
@@ -1,6 +1,7 @@
 package org.vhn.ecommerce.product.service.core.seller
 
 import jakarta.annotation.PostConstruct
+import jakarta.persistence.criteria.Predicate
 import org.springframework.expression.Expression
 import org.springframework.stereotype.Service
 import java.time.OffsetDateTime
@@ -12,9 +13,14 @@ class ProductSellerService(val productSellerRepository: ProductSellerRepository)
     @PostConstruct
     fun initSeller() = productSellerRepository.save(ProductSeller(UUID.randomUUID(), OffsetDateTime.now(), "abc"))
 
-    fun getSellers() = productSellerRepository.findAll();
+    fun getSellers(): MutableIterable<ProductSeller> = productSellerRepository.findAll();
 
-    fun getSellers(name: String) = productSellerRepository.findAll { root, query, criteriaBuilder ->
-        criteriaBuilder.equal(root.get<Expression>("name"), name)
-    }
+    fun getSellers(name: String?): MutableList<ProductSeller> =
+        productSellerRepository.findAll { root, _, criteriaBuilder ->
+            val predicates = ArrayList<Predicate>()
+            if (name != null) {
+                predicates.add(criteriaBuilder.equal(root.get<Expression>("name"), name))
+            }
+            criteriaBuilder.and(*predicates.toTypedArray())
+        }
 }
--- product-service/inbound/src/main/kotlin/org/vhn/ecommerce/product/service/inbound/ProductSellerController.kt
+++ product-service/inbound/src/main/kotlin/org/vhn/ecommerce/product/service/inbound/ProductSellerController.kt
@@ -3,11 +3,12 @@ package org.vhn.ecommerce.product.service.inbound
 import org.springframework.graphql.data.method.annotation.Argument
 import org.springframework.graphql.data.method.annotation.QueryMapping
 import org.springframework.stereotype.Controller
+import org.vhn.ecommerce.product.service.core.seller.ProductSeller
 import org.vhn.ecommerce.product.service.core.seller.ProductSellerService
 
 @Controller
 class ProductSellerController(val productSellerService: ProductSellerService) {
 
     @QueryMapping
-    fun sellers(@Argument name: String) = productSellerService.getSellers(name)
+    fun sellers(@Argument name: String?): MutableList<ProductSeller> = productSellerService.getSellers(name)
 }
